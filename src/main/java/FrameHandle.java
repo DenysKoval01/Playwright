0e1bb6fb290f8fad7523cf245a2775493e25f1a9
DenysKoval01 <denys.koval.qa@gmail.com>
2023-11-07 14:32:05 +0200

    Playwright frames

---
+++ src/main/java/FrameHandle.java
@@ -0,0 +1,31 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class FrameHandle {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+
+        BrowserType.LaunchOptions lp = new BrowserType.LaunchOptions();
+        lp.setHeadless(false);
+
+        Browser browser = playwright.chromium().launch(lp);
+        Page page = browser.newPage();
+        /*page.navigate("http://www.londonfreelance.org/courses/frames/index.html");*/
+/*        String header = page.frameLocator("frame[name='main']").locator("h2").textContent();
+        System.out.println(header);*/
+
+/*        String h2 = page.frame("main").locator("h2").textContent();
+        System.out.println(h2);*/
+
+      /*  String headerXPATH = page.frameLocator("//frame[@name='main']").locator("h2").textContent();
+        System.out.println(headerXPATH);*/
+
+        page.navigate("https://www.formsite.com/templates/registration-form-templates/vehicle-registration-form/");
+
+        page.locator("img[title='Vehicle-Registration-Forms-and-Examples']").click();
+        page.frameLocator("//iframe[contains(@id,'frame-one')]").locator("#RESULT_TextField-8")
+                .fill("Denys" );
+    }
+}
